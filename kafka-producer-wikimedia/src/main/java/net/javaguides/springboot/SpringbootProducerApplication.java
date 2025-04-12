4bdfd811b58aaad8045aba4e522a9daa9914ce69
Serhii <seleon000@gmail.com>
2025-04-12 21:36:08 +0200

    add wikimedia event handler and producer

--- kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/SpringbootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/SpringbootProducerApplication.java
@@ -1,12 +1,24 @@
 package net.javaguides.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootProducerApplication {
+public class SpringbootProducerApplication implements CommandLineRunner {
+
+    private final WikimediaChangesProducer wikimediaChangesProducer;
+
+    public SpringbootProducerApplication(WikimediaChangesProducer wikimediaChangesProducer) {
+        this.wikimediaChangesProducer = wikimediaChangesProducer;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
